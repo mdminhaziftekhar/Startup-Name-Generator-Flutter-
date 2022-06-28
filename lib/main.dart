a35d96370a06b75edfd07bf99de95e02e3893c92
Minhaz Iftekhar <mdminhaziftekhar@gmail.com>
2022-06-28 20:14:38 +0600

    Infinite Scroll added along with random words

---
+++ lib/main.dart
@@ -0,0 +1,59 @@
+import 'package:flutter/material.dart';
+import 'package:english_words/english_words.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final wordPair = WordPair.random();
+    return MaterialApp(
+      title: 'Startup Name Generator',
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('Infinite Names'),
+        ),
+        body: const Center(
+          child: RandomWords(),
+        ),
+      ),
+    );
+  }
+}
+
+class RandomWords extends StatefulWidget {
+  const RandomWords({Key? key}) : super(key: key);
+
+  @override
+  State<RandomWords> createState() => _RandomWordsState();
+}
+
+class _RandomWordsState extends State<RandomWords> {
+  final _suggestions = <WordPair>[];
+  final _biggerFont = const TextStyle(fontSize: 18);
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView.builder(
+      padding: const EdgeInsets.all(16.0),
+      itemBuilder: /*1*/ (context, i) {
+        if (i.isOdd) return const Divider(); /*2*/
+
+        final index = i ~/ 2; /*3*/
+        if (index >= _suggestions.length) {
+          _suggestions.addAll(generateWordPairs().take(10)); /*4*/
+        }
+        return ListTile(
+          title: Text(
+            _suggestions[index].asPascalCase,
+            style: _biggerFont,
+          ),
+        );
+      },
+    );
+  }
+}
